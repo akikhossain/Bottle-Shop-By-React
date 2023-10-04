6b8e41952b565b1028a361934f194ebcdea0acf8
akik-hossain <akikhs00@gmail.com>
2023-10-04 23:46:31 +0600

    create bottle list

---
+++ src/Components/Bottle/Bottle.jsx
@@ -0,0 +1,30 @@
+import "./Bottle.css";
+
+const Bottle = ({ bottle }) => {
+  const {
+    img,
+    name,
+    price,
+    quantity,
+    category,
+    seller,
+    shipping,
+    ratings,
+    ratingsCount,
+  } = bottle;
+  return (
+    <div className="bottle">
+      <img src={img} alt="" />
+      <h2>Bottle name: {name} </h2>
+      <h2>Price: ${price}</h2>
+      <h2>Category: {category}</h2>
+      <h2>Seller: {seller}</h2>
+      <h2>Shipping: {shipping}</h2>
+      <h2>Ratings: {ratings}</h2>
+      <h2>RatingsCount: {ratingsCount}</h2>
+      <h2>Quantity: {quantity}</h2>
+    </div>
+  );
+};
+
+export default Bottle;
---
+++ src/Components/Bottles/Bottles.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
+
+const Bottles = () => {
+  const [bottles, setBottles] = useState([]);
+  useEffect(() => {
+    fetch("Bottle.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+  return (
+    <div>
+      <h2>Name: {bottles.length}</h2>
+      <div className="bottle-container">
+        {bottles.map((bottle) => (
+          <Bottle key={bottle.id} bottle={bottle}></Bottle>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Bottles;
